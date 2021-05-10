f6ccb1a858bb93b5a281aa4a064f709772519a5f
xb <xb1231>
2021-05-10 12:10:58 +0800

    v1

---
+++ src/Encryption.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @CreateTime:   2021-05-10 23:55
+ * @Author:       kk  <1228196390@qq.com>
+ * @Copyright:    copyright(2020) Easyswoole all rights reserved
+ * @Description:  编码工具
+ */
+namespace EasySwoole\Test;
+
+class Encryption
+{
+
+    public function base64UrlEncode($content)
+    {
+        return $content;
+    }
+
+}
